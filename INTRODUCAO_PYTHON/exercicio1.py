f48877f885524cc689c27e4854146e403b1c6430
Caua-amestrong <caua.silveira@escola.pr.gov.br>
2025-04-10 15:03:35 -0300

    teste de commit

---
+++ INTRODUCAO_PYTHON/exercicio1.py
@@ -0,0 +1,5 @@
+nome = "alice"
+idade =25
+altura = 1.68
+print("Olá, meu nome é",nome,"e tenho", idade,"anos" )
+print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.")
---
+++ INTRODUCAO_PYTHON/ola.py
@@ -0,0 +1,2 @@
+print("hello world")
+print("Neste curso, voce aprendera python")
