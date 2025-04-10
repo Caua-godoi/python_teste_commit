e03552e234c302caf02f86b3ad6b39c4c4f47ff9
Caua-amestrong <caua.silveira@escola.pr.gov.br>
2025-04-10 15:27:48 -0300

    adicionando comentarios

--- INTRODUCAO_PYTHON/exercicio1.py
+++ INTRODUCAO_PYTHON/exercicio1.py
@@ -1,5 +1,6 @@
-nome = "alice"
-idade =25
-altura = 1.68
+#lista de objetos
+nome = "alice" #variavel string
+idade =25 #variavel inteira (int)
+altura = 1.68 #variavel float
 print("Olá, meu nome é",nome,"e tenho", idade,"anos" )
-print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.")
+print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.") #executando a aplicação
