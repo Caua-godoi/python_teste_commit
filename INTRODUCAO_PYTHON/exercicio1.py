feac326daab8ce72ee1cfa3bd4b89646565a505b
Caua-amestrong <caua.silveira@escola.pr.gov.br>
2025-04-10 15:44:07 -0300

    variaveis e dados

--- INTRODUCAO_PYTHON/exercicio1.py
+++ INTRODUCAO_PYTHON/exercicio1.py
@@ -4,4 +4,3 @@ idade =25 #variavel inteira (int)
 altura = 1.68 #variavel float
 print("Olá, meu nome é",nome,"e tenho", idade,"anos" )
 print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.") #executando a aplicação
-#realmente funciona??
